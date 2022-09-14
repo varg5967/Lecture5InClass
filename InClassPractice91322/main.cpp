02891b6122ce7e61cec61160e820c794061ea220
varg5967 <varg5967@pacificu.edu>
2022-09-14 11:24:28 -0700

    Add project files.

---
+++ InClassPractice91322/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int score1, score2, score3, sum=0 ; 
+	double average;
+	
+	cout << "Enter first score: "; 
+	cin >> score1;
+	cout << "Enter second score: ";
+	cin >> score2; 
+	sum = score1 + score2;
+	cout << "Enter third score: ";
+	cin >> score3;
+	sum = sum + score3;
+
+	average = (score1+score2+score3)/(3)
+
+	if (average >= 
+
+
+
+
+	return EXIT_SUCCESS;
+}
