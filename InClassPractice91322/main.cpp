2a35ab4e866291e7dfa1690f5b145a5f51e25ff4
varg5967 <varg5967@pacificu.edu>
2022-09-16 11:01:30 -0700

    finished code - exam scores

--- InClassPractice91322/main.cpp
+++ InClassPractice91322/main.cpp
@@ -4,22 +4,42 @@ using namespace std;
 
 int main() {
 	
-	int score1, score2, score3, sum=0 ; 
-	double average;
+	double score;
+	double scoreSum = 0.0;
+	double scoreAverage; 
+
+	int count = 0; 
+
+	const int PASS_AVERAGE_CUTOFF = 60; 
 	
-	cout << "Enter first score: "; 
-	cin >> score1;
-	cout << "Enter second score: ";
-	cin >> score2; 
-	sum = score1 + score2;
-	cout << "Enter third score: ";
-	cin >> score3;
-	sum = sum + score3;
+	cout << "Enter first exam score: "; 
+	cin >> score;
+	
+	scoreSum = scoreSum + score;
+	count = count + 1; 
 
-	average = (score1+score2+score3)/(3)
 
-	if (average >= 60) 
+	cout << "Enter second exam score: ";
+	cin >> score; 
 
+	scoreSum = scoreSum + score; 
+	count = count + 1;
+
+	cout << "Enter third exam score: ";
+	cin >> score;
+	
+	scoreSum = scoreSum + score;
+	count = count + 1; 
+
+	scoreAverage = scoreSum / count;
+
+	if (scoreAverage > PASS_AVERAGE_CUTOFF) {
+		cout << "You Passed";
+	}
+
+	if (scoreAverage <= PASS_AVERAGE_CUTOFF) {
+		cout << "You Failed";
+		}
 
 
 
